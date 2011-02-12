5859f0f1568f2778bedc9de0d3aa6a3e33f0dde8
Dustin Smith <dustinsmith1024@hotmail.com>
2011-02-12 14:35:19 -0600

    initlal rails build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paleo::Application.config.secret_token = 'b5248a66ed2f17f55a51378ea1d24c42d32b081395a0510d4155c55a28026bf71ac318bc209ab1c0552084804dd8eeb6a43ee19489eefdda209c01e82147e379'
